89ef0a481e3949b70d753d576d6ff1908fafb44c
vivienvo <42749976+vivienvo@users.noreply.github.com>
2019-01-26 23:09:04 -0500

    Added a greeting using React

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+
 import './App.css';
 import NameForm from './NameForm';
-// import NameForm from './NameForm';
+
 class App extends Component {
-  render() {
-    return (
-      <NameForm />
-    );
+  constructor(props) {
+    super(props);
+    this.setValid = this.setValid.bind(this);
+    this.state = {isValid:true};
   }
-}
+  setValid(value)
+  {
+    this.state.isValid = ({isValid:value});
+    console.log("Getting input")
+  }
+  render() {
+    return(<div> <NameForm  setValid={this.Setvalid}/>
+    {(!this.state.isValid) ?
+      <div class="errortext">Error not alpha input</div>
+    : null}
+    </div>)
+    }
+  }
+
 
 export default App;
--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -1,34 +1,45 @@
 import React from 'react';
+
 class NameForm extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  constructor(props) {
+    super(props);
+    this.state = {value: '', nameAvailable:false, isValid:true, setValid:props.setValid};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+    handleSubmit(event) {
+    //alert('Welcome and Greetings, ' + this.state.value);
+    if (!/[^a-zA-Z]+/.test(this.state.value))
+    this.setState({nameAvailable: true});
+  }
+  else () {
+    this.state.setValid.setState(false);
+  }
 
-      handleChange(event) {
-        this.setState({value: event.target.value});
-        this.name = event.target.value;
-      }
 
-      handleSubmit(event) {
-
-      }
-
-      render() {
+  render() {
+    if(!this.state.nameAvailable) {
         return (
           <form onSubmit={this.handleSubmit}>
             <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
             </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
+          <input type="submit" value="Submit" />
+          {(!this.state.isValid) ?
+          <p key="Error" class="errorText">Must be a valid a-z or A-Z</p>
+          : null}
+          </form>);
+        } else {
+          return (<div>good morning {this.save.value}</div>);
+        }
       }
     }
 
 
-export default NameForm;
+
+  export default NameForm;
